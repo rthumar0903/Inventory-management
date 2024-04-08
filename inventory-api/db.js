f67a824d2c688b1f7ef04c232248011e24b2abdf
rthumar0903 <thumarraj999@gmail.com>
2024-04-08 19:48:34 +0530

    changes to db connection string

--- inventory-api/db.js
+++ inventory-api/db.js
@@ -1,5 +1,5 @@
 const Mongoose = require("mongoose")
-const localDB = `mongodb+srv://thumarraj999:G7CS5A5HhCT43wvl@cluster0.qjym0ml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const localDB = 'your_connection_string'
 const connectDB = async () => {
   await Mongoose.connect(localDB, {
     useNewUrlParser: true,
